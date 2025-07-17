4c38267012b2615c15a1ead7632ac54c287cc3a6
Anjana Tissera <tisserapac@yahoo.com>
2025-07-17 22:31:53 +0700

    Completed upto section 7 - lesson 64

---
+++ discuss/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/react";
+import { div } from "framer-motion/client";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
