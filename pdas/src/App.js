34fd661b50a415f108b65683968253d8c9aba61f
Anjana Tissera <anjanat@ait.asia>
2025-07-31 16:39:31 +0700

    Completed lesson 152

---
+++ pdas/src/App.js
@@ -0,0 +1,16 @@
+import ProfileCard from "./ProfileCard";
+
+function App() {
+  return (
+    <div>
+      <div>Persoanl Digital Assistants</div>
+      <div>
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pdas/src/ProfileCard.js
@@ -0,0 +1,5 @@
+function ProfileCard() {
+  return <div>Profile card</div>;
+}
+
+export default ProfileCard;
